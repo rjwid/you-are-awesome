c0c5f3b26613dccb3becd80e55379400f8209d04
RJ Widrow <rwidrow@yahoo.com>
2025-08-09 14:23:58 -0700

    Initial Commit

---
+++ you are awesome/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  you are awesome
+//
+//  Created by Robert Widrow on 8/9/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
